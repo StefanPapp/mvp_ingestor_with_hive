4c2b9d9cb148f9634b3fe39b304f03688576f56d
Stefan <stefan.papp@icloud.com>
2015-07-28 13:36:49 +0200

    Added some changes

--- HadoopUpload.py
+++ HadoopUpload.py
@@ -1,20 +1,41 @@
+"""
+ Upload from Edge to HDFS
+
+ Author: Stefan Papp
+
+"""
+
 import os
+import subprocess
 import logging
 
+
 class HadoopUpload(object):
-    landing_directory = ''
-    source_directory = ''
+    hdfs_base_dir = ""
+    edge_base_dir = ""
+    file_categories = []
 
     def set_dir(self, sd, ld):
-        self.landing_directory = ld
-        self.source_directory = sd
+        self.hdfs_base_dir = ld
+        self.edge_base_dir = sd
+
+    def set_file_cat(self, fc):
+        self.file_categories = fc
 
     def upload_hadoop(self):
-	logger = logging.getLogger("Ingest")
-	logger.info("source directory: " + self.source_directory)
-	all_objects = os.listdir(self.source_directory)
-        for current_file in all_objects:
-            os.system('hdfs dfs -put ' + current_file + ' ' + self.landing_directory)
-	        os.system('pig -useHCatalog -p INPUTFILE=' + current_file + ' -p LANDDIR=' + self.landing_directory + ' pig_convert.pig')
+        logger = logging.getLogger("Ingest")
+        logger.info("setting base source directory: " + self.edge_base_dir)
 
+        for fc in self.file_categories:
+            source_directory = self.edge_base_dir + "/" + fc.get_sub_dir
+            destination_dir = self.hdfs_base_dir + "/" + fc.get_sub_dir
+            all_objects = os.listdir(source_directory)
+            for current_file in all_objects:
+                try:
+                    subprocess.call('hdfs dfs -put ' + current_file + ' ' + destination_dir, shell= True)
+                except OSError as e:
+                    logger.error("Could not upload file to hadoop " + current_file + ' ' + destination_dir)
+
+                # os.system('pig -useHCatalog -p INPUTFILE=' + current_file + ' -p LANDDIR=' + destination_dir + ' ' + fc.get_file_type + '.pig')
+                subprocess.call('pig -useHCatalog -p INPUTFILE=' + current_file + ' -p LANDDIR=' + destination_dir + ' ' + fc.get_file_type + '.pig', shell= True)
 
--- SshTransfer.py
+++ SshTransfer.py
@@ -1,42 +1,83 @@
+"""
+ Transfers Files from Remote server to Edge node
+
+ Author: Stefan Papp
+
+"""
+
 import paramiko
 import logging
+
 
 class SshTransfer(object):
 
     server = ""
     username = ""
     password = ""
-    remote_path = ""
-    local_path = ""
+    remote_base_dir = ""
+    edge_base_dir = ""
+    file_categories = []
 
     def set_connection(self, server, username, password, remote_path, local_path):
         self.server = server
         self.username = username
         self.password = password
-        self.remote_path = remote_path
-        self.local_path = local_path
+        self.remote_base_dir = remote_path
+        self.edge_base_dir = local_path
 
-    def download_all_files(self):
+    def set_file_cat(self, fc):
+        self.file_categories = fc
+
+    def download_files(self):
         ssh = paramiko.SSHClient()
         ssh.load_system_host_keys()
 
         logger = logging.getLogger("Ingest")
-        logger.info(self.server + " " + self.username + " " + self.password)
-        ssh.connect(self.server, username=self.username, password=self.password)
-        logger.info("ssh connected")
+        logger.debug("INFO: Logging to SSH with Server, Username " +  self.server + " " + self.username)
+        try:
+            ssh.connect(self.server, username=self.username, password=self.password)
+        except paramiko.BadHostKeyException:
+            logger.error("the server’s host key could not be verified")
+        except paramiko.AuthenticationException:
+            logger.error("authentication failed")
+        except paramiko.SSHException:
+            logger.error("other SSH Exception")
 
+        logger.info("SUCCESS: SSH connected established")
+
+        logger.debug("INFO: Open SFTP")
         sftp = ssh.open_sftp()
-        logger.info("sftp opened. using + " + self.remote_path)
-        all_objects = sftp.listdir(self.remote_path)
+        logger.info("SUCCESS: sftp opened. using directory + " + self.remote_base_dir)
 
-        logger.info("parsing all files")
-        for afile in all_objects:
-            print(afile)
-            sftp.get(self.remote_path + '/' + afile, self.local_path + '/'+ afile)
+        logger.debug("INFO: Downloading files to edge node")
 
+        for fc in self.file_categories:
+            source_directory = self.remote_base_dir + "/" + fc.get_sub_dir
+            destination_directory = self.edge_base_dir + "/" + fc.get_sub_dir
+            all_objects = sftp.listdir(source_directory)
+            for current_file in all_objects:
+                sftp.get(source_directory + '/' + current_file, destination_directory + '/'+ current_file)
+                logger.info("SUCCESS: transferred file " + source_directory + '/' + current_file)
+                if fc.get_archive_action == "keep":
+                    try:
+                        sftp.rename(source_directory + '/' + current_file, source_directory + '/archive/' + current_file)
+                    except IOError:
+                        logger.error("ERROR: Could not move file to archive " + source_directory + '/' + current_file)
+                    logger.info("SUCCESS: file archived to " + source_directory + '/' + current_file, source_directory + '/archive/' + current_file)
+                else:
+                    try:
+                        sftp.remove(source_directory + '/' + current_file)
+                    except IOError:
+                        logger.error("ERROR: Could not remove file " + source_directory + '/' + current_file)
+                    logger.info("SUCCESS: file deleted")
+
+        logger.debug("INFO: Closing sftp")
         sftp.close()
-        logger.info("sftp closed")
+        logger.info("SUCCESS: sftp closed")
+
+        logger.debug("INFO: Closing ssh")
         ssh.close()
+        logger.info("SUCCESS: ssh closed")
 
 
 
